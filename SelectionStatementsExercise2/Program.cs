32c15645894cbbf957c8c1c34f232f3c7341ab04
Adam Jankowski <ghost17814@gmail.com>
2023-04-12 14:09:27 -0400

    first and final commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("So what was your favorite subject in school?");
+            
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Gross! That's the worst!");
+                    break;
+            }
+            switch (subject.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("Ah, yes yes. A fine selection.");
+                    break;
+            }
+            switch (subject.ToLower())
+            {
+                case "writing":
+                    Console.WriteLine("Objectively the best choice!");
+                    break;
+            }
+            switch (subject.ToLower())
+            {
+                case "Art":
+                    Console.WriteLine("Could be better could be worse, if finger painting is \n" +
+                        "involved then count me in!");
+                    break;
+            }
+                        switch (subject.ToLower())
+            {
+                default:
+                    Console.WriteLine("Sorry... I wish lunch or gym counted too.. :(");
+                    break;
+            }
         }
     }
 }
